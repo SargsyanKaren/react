683291c882913e85e32de878b17288cc733af93c
SargsyanKaren <ksarg9618@gmail.com>
2020-02-02 21:34:40 +0300

    Add View Componnet responsive

--- src/styles/styled-components/View/index.js
+++ src/styles/styled-components/View/index.js
@@ -1,74 +1,89 @@
 import styled from 'styled-components';
 
-const size = ({ w, h, minW, maxW, minH, maxH, lh }) => `
-  ${ w ? `width: ${w}` : ""};
-  ${ h ? `height: ${h}` : ""};
+const size = ({ 
+  minW, 
+  maxW, 
+  minH, 
+  maxH, 
+  width, 
+  height,
+}) => `
+  ${ width ? `width: ${width}` : ""};
   ${ minW ? `min-width: ${minW}` : ""};
   ${ maxW ? `max-width: ${maxW}` : ""};
+  ${ height ? `height: ${height}` : ""};
   ${ minH ? `min-height: ${minH}` : ""};
   ${ maxH ? `max-height: ${maxH}` : ""};
 `;
 
-const marginPadding = ({ m, p, mT, mB, mL, mR, pT, pB, pL, pR }) => `
-  ${ m ? `margin: ${m}` : ""};
-  ${ p ? `padding: ${p}` : ""};
-  ${ mT ? `margin-top: ${mT}` : ""};
-  ${ mB ? `margin-bottom: ${mB}` : ""};
-  ${ mL ? `margin-left: ${mL}` : ""};
-  ${ mR ? `margin-right: ${mR}` : ""};
-  ${ pT ? `padding-top: ${pT}` : ""};
-  ${ pB ? `padding-bottom: ${pB}` : ""};
-  ${ pL ? `padding-left: ${pL}` : ""};
-  ${ pR ? `padding-right: ${pR}` : ""};
+const marginPadding = ({ 
+  margin, 
+  padding, 
+  marginT, 
+  marginB, 
+  marginL, 
+  marginR, 
+  paddingT, 
+  paddingB, 
+  paddingL, 
+  paddingR, 
+}) => `
+  ${ margin ? `margin: ${margin}` : ""};
+  ${ padding ? `padding: ${padding}` : ""};
+  ${ marginT ? `margin-top: ${marginT}` : ""};
+  ${ marginB ? `margin-bottom: ${marginB}` : ""};
+  ${ marginL ? `margin-left: ${marginL}` : ""};
+  ${ marginR ? `margin-right: ${marginR}` : ""};
+  ${ paddingT ? `padding-top: ${paddingT}` : ""};
+  ${ paddingB ? `padding-bottom: ${paddingB}` : ""};
+  ${ paddingL ? `padding-left: ${paddingL}` : ""};
+  ${ paddingR ? `padding-right: ${paddingR}` : ""};
 `;
 
-const fonts = ({ fontF, fontSize, fontW, fontStyle, fontStretch }) => `
-  ${ fontF ? `font-family: ${fontF}` : ""};
-  ${ fontSize ? `font-size: ${fontSize}` : ""};
-  ${ fontW ? `font-weight: ${fontW}` : ""};
-  ${ fontStyle ? `font-style: ${fontStyle}` : ""};
-  ${ fontStretch ? `font-stretch: ${fontStretch}` : ""};
-`;
-
-const text = ({ tTf, whiteSpace, tabSize, lBreak, overflowWrap, tAlign, tD, tDLine, tDStyle, tDColor, tSh, tUP }) => `
-  ${ tTf ? `text-transform: ${tTf}` : ""};
-  ${ whiteSpace ? `white-space: ${whiteSpace}` : ""};
-  ${ tabSize ? `tab-size: ${tabSize}` : ""};
-  ${ lBreak ? `line-break: ${lBreak}` : ""};
-  ${ overflowWrap ? `overflow-wrap: ${overflowWrap}` : ""};
-  ${ tAlign ? `text-align: ${tAlign}` : ""};
-  ${ tD ? `text-decoration: ${tD}` : ""};
-  ${ tDLine ? `text-decoration-line: ${tDLine}` : ""};
-  ${ tDStyle ? `text-decoration-style: ${tDStyle}` : ""};
-  ${ tDColor ? `text-decoration-color: ${tDColor}` : ""};
-  ${ tSh ? `text-shadow: ${tSh}` : ""};
-  ${ tUP ? `text-underline-position: ${tUP}` : ""};
-`;
-
-const positions = ({ pos, top, right, bottom, left, float, z }) => `
-  ${ pos ? `position: ${pos}` : ""};
+const positions = ({ 
+  top, 
+  left, 
+  right, 
+  bottom, 
+  zIndex, 
+  position, 
+}) => `
   ${ top ? `top: ${top}` : ""};
+  ${ left ? `left: ${left}` : ""};
   ${ right ? `right: ${right}` : ""};
   ${ bottom ? `bottom: ${bottom}` : ""};
-  ${ left ? `left: ${left}` : ""};
-  ${ float ? `float: ${float}` : ""};
-  ${ z ? `z-index: ${z}` : ""}
+  ${ zIndex ? `z-index: ${zIndex}` : ""}
+  ${ position ? `position: ${position}` : ""};
 `;
 
-const border = ({ border, bW, bT, bB, bL, bR, bStyle, bRad, captionS, bColl, bColor, bSpac, emptyC }) => `
+const border = ({ 
+  border, 
+  emptyC, 
+  radius, 
+  borderW, 
+  borderT, 
+  borderB, 
+  borderL, 
+  borderR, 
+  captionS, 
+  borderColl, 
+  borderSpac, 
+  borderColor, 
+  borderStyle, 
+}) => `
   ${ border ? `border: ${border}` : ""};
-  ${ bW ? `border-width: ${bW}` : ""};
-  ${ bT ? `border-top: ${bT}` : ""};
-  ${ bB ? `border-bottom: ${bB}` : ""};
-  ${ bL ? `border-left: ${bL}` : ""};
-  ${ bR ? `border-right: ${bR}` : ""};
-  ${ bStyle ? `border-style: ${bStyle}` : ""};
-  ${ bRad ? `border-radius: ${bRad}` : ""};
-  ${ captionS ? `caption-side: ${captionS}` : ""};
-  ${ bColl ? `border-collapse: ${bColl}` : ""};
-  ${ bColor ? `border-color: ${bColor}` : ""}
-  ${ bSpac ? `border-spacing: ${bSpac}` : ""};
+  ${ borderT ? `border-top: ${bT}` : ""};
+  ${ borderL ? `border-left: ${bL}` : ""};
+  ${ borderR ? `border-right: ${bR}` : ""};
+  ${ borderW ? `border-width: ${bW}` : ""};
+  ${ borderB ? `border-bottom: ${bB}` : ""};
   ${ emptyC ? `empty-cells: ${emptyC}` : ""};
+  ${ radius ? `border-radius: ${radius}` : ""};
+  ${ captionS ? `caption-side: ${captionS}` : ""};
+  ${ borderStyle ? `border-style: ${bStyle}` : ""};
+  ${ borderColor ? `border-color: ${bColor}` : ""};
+  ${ borderSpac ? `border-spacing: ${bSpac}` : ""};
+  ${ borderColl ? `border-collapse: ${bColl}` : ""};
 `;
 
 const backgrounds = ({
@@ -145,19 +160,46 @@ const flexBox = ({
   ${ columnReverse ? 'flex-direction: column-reverse;' : '' }
 `;
 
-const other = ({ boxS, boxSh, overflow, c, op, tr }) => `
-  ${ boxS ? `box-sizing: ${boxS}` : ""};
-  ${ boxSh ? `box-shadow: ${boxSh}` : ""};
+const other = ({ boxSizing, shadow, overflow, color, opacity, transition }) => `
+  ${ color ? `color: ${color}` : ""};
+  ${ opacity ? `opacity: ${opacity}` : ""};
+  ${ shadow ? `box-shadow: ${shadow}` : ""};
   ${ overflow ? `overflow : ${overflow}` : ""};
-  ${ c ? `color: ${c}` : ""};
-  ${ op ? `opacity: ${op}` : ""};
-  ${ tr ? `transition: ${tr}` : ""};
+  ${ boxSizing ? `box-sizing: ${boxSizing}` : ""};
+  ${ transition ? `transition: ${transition}` : ""};
 `;
+
+const setResponsiveStyles = props => {
+  const mediaQuerys = {
+    s: '(max-width: 480px)',
+    m: '(min-width: 480px) and (max-width: 960px)',
+    l: '(min-width: 960px) and (max-width: 1260px)',
+    xl: '(min-width: 1800px)'
+  };
+
+  let responsiveProps = Object.entries(props).filter(prop => prop[0].includes('_'));
+  if(!responsiveProps.length) return '';
+
+  const query = {};
+
+  responsiveProps.forEach(item => {
+    const queryName = item[0].slice(item.indexOf('-'));
+    const propName = item[0].slice(0, item.indexOf('-') - 1);
+
+    if(!query[queryName]) query[queryName] = {};
+    
+    query[queryName][propName] = item[1];
+  })
+
+  return Object.entries(query).map(item => `
+    @media ${mediaQuerys[item[0]]}{
+      ${setStyles(item[1])}
+    }
+  `).join('');
+};
 
 const View = styled.div`
   ${ size}
-  ${ fonts}
-  ${ text}
   ${ marginPadding}
   ${ positions}
   ${ backgrounds}
